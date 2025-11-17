2a03081c9620e69d463d84195f708993f25a0cf7
Alfonso Alvarez <ponchoalv@gmail.com>
2025-11-17 19:32:23 +0000

    improved part 2 implementing a solver algorithm (newton's method)

--- src/2022/day21/README.md
+++ src/2022/day21/README.md
@@ -58,10 +58,24 @@ getNumber(monkeyName: string): number {
 
 ```
 
+recursively look for the root number.
+
 ## Part 2
 
-[Description of part 2]
+Part 2 was harder.
+
+I optimise the recursive function to use memo for all depth's after `humn` was spotter which wouldn't be affected, and then with if / else to step faster I found the value in arround 8s.
+On the second optmization I read about solvers and implement a naive version of newton's method (Newton–Raphson method) which require some derivative calculation.
+
+```
+f'(x) = (f(x + h) - f(x)) / h
+```
+
+Set the the H value in something high, like 1, because we are looking for bigs numbers (from force brute 1st iteration I know that).
+
+Now the problem gets solve in around 5ms (with an m4 macbook / mac mini)
 
 ## Notes
 
-- [Any optimization notes or algorithm explanations]
+- this video was super helpfull to visualize how newton's method work [video](https://www.youtube.com/watch?v=1uN8cBGVpfs)
+- wikipedia article also usefull [link](https://en.wikipedia.org/wiki/Newton%27s_method)
--- src/2022/day21/solution.ts
+++ src/2022/day21/solution.ts
@@ -24,6 +24,7 @@ export default class Day21Solution extends BaseSolution {
   private human: number = 0;
   private humanDepth: number = 0;
   private humanDepthFound: boolean = false;
+  private EPSILON = 0.5; // Small step for derivative approximation (cannot be 1 because would make test.txt failed)
 
   private parseMonkeyList(input: string) {
     const lines = this.lines(input);
@@ -163,27 +164,48 @@ export default class Day21Solution extends BaseSolution {
     return this.getNumberForMonkey("root");
   }
 
-  part2(input: string, isTest: boolean = false): string | number {
-    this.parseMonkeyList(input);
-    let diff = this.getNumbersForRoot("root", 0);
-    while (diff !== 0) {
-      // if (diff % 10000) {
-      //   console.log(this.human, diff);
-      // }
+  private approximateDerivative(func: (num: number) => number, x: number) {
+    return (func(x + this.EPSILON) - func(x)) / this.EPSILON;
+  }
 
-      if (diff > 100000000) {
-        this.human += 100000000;
-      } else if (diff > 1000000) {
-        this.human += 1000000;
-      } else if (diff < 0 && Math.abs(diff) > 10000) {
-        this.human -= 10000;
-      } else {
-        this.human += 1;
+  private newtonSolver(
+    func: (num: number) => number,
+    initialGuess: number,
+    tolerance: number = 1e-6,
+    maxIterations: number = 100
+  ): number | null {
+    let x = initialGuess;
+
+    for (let i = 0; i < maxIterations; i++) {
+      let fX = func(x);
+      let fPrimeX = this.approximateDerivative(func, x);
+
+      // Check for convergence
+      if (Math.abs(fX) < tolerance) {
+        return x;
       }
 
-      diff = this.getNumbersForRoot("root", 0);
+      // Check for zero derivative (a flat spot, which makes the method fail)
+      if (Math.abs(fPrimeX) < this.EPSILON) {
+        console.error("Newton's method failed due to zero derivative.");
+        return null;
+      }
+
+      // Newton's Formula: x_n+1 = x_n - f(x_n) / f'(x_n)
+      x = x - fX / fPrimeX;
     }
 
-    return this.human;
+    console.warn("Max iterations reached.");
+    return x;
+  }
+
+  part2(input: string, isTest: boolean = false): string | number {
+    this.parseMonkeyList(input);
+    const targetFunc = (guess: number) => {
+      this.human = guess;
+      return this.getNumbersForRoot("root", 0);
+    };
+
+    return Math.round(this.newtonSolver(targetFunc, 200, 0.5, 100) ?? 0);
   }
 }
