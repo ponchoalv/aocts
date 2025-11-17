9d49db3d4bf2403c0d9354d896901b9943196cc8
Alfonso Alvarez <ponchoalv@gmail.com>
2025-11-17 12:29:37 +0000

    finished part 2

--- src/2022/day20/solution.ts
+++ src/2022/day20/solution.ts
@@ -53,8 +53,54 @@ export default class Day20Solution extends BaseSolution {
 
   part2(input: string, isTest: boolean = false): string | number {
     const lines = this.lines(input);
+    const elems: [number, number][] = lines.map((line, idx) => [
+      parseInt(line.trim()) * 811589153,
+      idx,
+    ]);
 
-    // TODO: Implement part 2
-    return 0;
+    const shuffled: [number, number][] = elems.map((arr) => [...arr]); // Deep copy
+    const size = elems.length;
+
+    for (let i = 0; i < 10; i++) {
+      for (const [value, initialIndex] of elems) {
+        // tengo que buscar el indice en el array donde se movió el elemento
+        let currentIdx = -1;
+        for (let i = 0; i < size; i++) {
+          if (shuffled[i]![1]! === initialIndex) {
+            currentIdx = i;
+            break;
+          }
+        }
+
+        if (currentIdx === -1) continue;
+        if (value === 0) continue; // 0 no se mueve
+
+        // Calcular el nuevo indice modulo arithmetic:
+        let moveDistance = value;
+        let newIdx = currentIdx + moveDistance;
+
+        newIdx = newIdx % (size - 1);
+
+        // Modulo en js, si es negativo, se mantiene negativo, así que hay que corregir
+        if (newIdx < 0) {
+          newIdx += size - 1;
+        }
+
+        // pslice se encarga de todo el trabajo de mover los elementos
+        const element: [number, number] = shuffled.splice(currentIdx, 1)[0]!; // elminar el element de la posicion actual
+        shuffled.splice(newIdx, 0, element); // agregar el elemento en su nueva posición
+      }
+    }
+
+    const zeroIndex = shuffled.findIndex((e: [number, number]) => e[0] === 0);
+
+    const sum = [1, 2, 3].reduce((acc, i) => {
+      const offset = 1000 * i;
+      const targetIndex = (zeroIndex + offset) % size;
+      const targetValue: number = shuffled[targetIndex]![0]!;
+      return acc + targetValue;
+    }, 0);
+
+    return sum;
   }
 }
